6cce14be4e80470b79eb79d9ec5cc0cb3261059d
Augusto Polonio <augustodrp@gmail.com>
2022-03-01 14:19:19 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,204 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 5.0f;
+    private float normalSpeed = 5.0f;
+    private float runSpeed = 15.0f;
+    private float jumpHeight = 1.0f;
+    public float shootForce = 1.0f;
+    private float gravityValue = -9.81f;
+    private bool isRunning = false;
+    public bool isHavingBall = false;
+    public Transform ball;
+    public float ballSpeed = 10f;
+
+    public Rigidbody rb;
+
+    public float distanceOfPlayer;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+
+        controller.enabled = false;
+    }
+
+    // private void LateUpdate()
+    // {
+    //     if (isHavingBall)
+    //     {
+    //         playerSpeed = 0f;
+    //         distanceOfPlayer = Vector3.Distance(ball.position, transform.position);
+
+    //         if (distanceOfPlayer > 5f)
+    //         {
+    //             transform.position = Vector3.MoveTowards(transform.position, ball.position, 15f * Time.deltaTime);
+    //         }
+    //     }
+    // }
+
+    private void FixedUpdate() {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        // print("move: " + move);
+        // print("move normalized: " + move.normalized);
+
+        rb.MovePosition(transform.position + move * playerSpeed * Time.deltaTime);
+
+        // if (move != Vector3.zero)
+        // {
+        //     rb.MovePosition(transform.position + transform.forward * 5.0f * Time.deltaTime);
+        // }
+    }
+
+    void Update()
+    {
+        
+
+        // if (isHavingBall)
+        // {
+        //     playerSpeed = 0f;            
+        // }
+
+        // distanceOfPlayer = Vector3.Distance(ball.position, transform.position);
+
+        // if (distanceOfPlayer > 5f)
+        // {
+        //     transform.position = Vector3.MoveTowards(transform.position, ball.position, 5f * Time.deltaTime);
+        // }
+
+        // if (distanceOfPlayer > 5f)
+        // {
+        //     //Follow the ball
+        //     //transform.LookAt(ball.parent.Find("playerLookAt"));
+        //     transform.Translate(Vector3.forward * 15f * Time.deltaTime);
+
+        // }
+        // if (distanceOfPlayer < 3.5f)
+        // {
+        //     //transform.LookAt(ball);
+        //     //Vector3 direction = ball.position - transform.position;
+
+        //     //transform.LookAt(ball);
+        //     //transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+
+
+
+        //     // if (distanceOfPlayer <= 3.5f)
+        //     // {
+        //     //     isHavingBall = false;
+        //     // }
+
+        //     //print("Direction: " + direction);
+        // }
+        // else
+        // {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isRunning = true;
+                playerSpeed = runSpeed;
+            }
+            else
+            {
+                isRunning = false;
+                playerSpeed = normalSpeed;
+            }
+
+            gameObject.transform.forward = move;
+
+            // if (isHavingBall)
+            // {
+            //     print("Carry ball!");
+            //     ball.GetComponent<SimplePlayerMove>().Move(300f, 1f);
+            // }
+
+            //ball.GetComponent<SimplePlayerMove>().Move(ballSpeed);
+        }
+
+        // Changes the height position of the player..
+        // if (Input.GetButtonDown("Jump") && groundedPlayer)
+        // {
+        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+
+        //     //GetComponent<Rigidbody>().velocity = transform.forward * shootForce;
+
+        //     //playerVelocity.x += Mathf.Sqrt(shootForce * 3.0f);
+        // }
+
+        // if (Input.GetKey(KeyCode.Space))
+        // {
+        //     rb.velocity = transform.forward * shootForce;
+        // }
+
+        // if (playerVelocity.x > 0)
+        // {
+        //     playerVelocity.x -= Time.deltaTime * shootForce;
+        // }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Ball" && !isHavingBall)
+        {
+            //this.enabled = false;
+            controller.enabled = false;
+            isHavingBall = true;
+            ball.GetComponent<SimplePlayerMove>().enabled = true;
+
+            //Join ball
+            SpringJoint joint = ball.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = new Vector3(0f, 0f, 2f);
+            // joint.connectedBody = transform.Find("Foot").GetComponent<Rigidbody>();
+            joint.connectedBody = rb;
+            joint.connectedMassScale = 20f;
+            // joint.massScale = .001f;
+            // joint.damper = 2f;
+            //joint.maxDistance = 1.5f;
+            //ball.GetComponent<SimplePlayerMove>().joint = joint;
+
+            ball.GetComponent<Rigidbody>().drag = 0;
+
+            /*
+            ball = other.transform;
+            print(other.transform.parent.Find("Direction"));
+            Vector3 oriPosition = transform.position;
+            transform.parent = other.transform.parent.Find("Direction");
+            other.transform.parent.Find("Direction").position = oriPosition;
+            other.transform.GetComponent<SimplePlayerMove>().enabled = true;
+            */
+        }
+    }
+
+    public void EnablePlayerToRun()
+    {
+        controller.enabled = true;
+        isHavingBall = false;
+        ball.GetComponent<SimplePlayerMove>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/SimplePlayerMove.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float maxSpeed = 10f;
+
+    private Rigidbody rb;
+    public float shootPower = 20f;
+    public float upForce = 20f;
+    //public float gravityValue;
+    public bool isShooting = false;
+    //public bool isMoving = false;
+    public bool isMovingBall = false;
+    public Transform direction;
+    public ForceMode forceModeShoot;
+    public ForceMode forceModeMovement;
+
+    //public SpringJoint joint;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            #region tests
+            //rb.velocity = transform.forward * shootPower;
+            //            rb.AddForce(transform.parent.forward * Time.deltaTime * shootPower * 1000f, ForceMode.Acceleration);
+
+            // rb.AddForce(transform.parent.forward * Time.deltaTime * shootPower * 1f, ForceMode.VelocityChange);
+            // rb.AddForce(transform.parent.up * Time.deltaTime * shootPower * 1f, ForceMode.VelocityChange);
+
+            // rb.AddForce(transform.parent.forward * Time.deltaTime * shootPower * 1f, ForceMode.Acceleration);
+            // rb.AddForce(transform.parent.up * Time.deltaTime * shootPower * 1f, ForceMode.Acceleration);
+
+            // rb.AddForce(transform.parent.forward * Time.deltaTime * shootPower * 20f, ForceMode.Impulse);
+            // rb.AddForce(transform.parent.up * Time.deltaTime * shootPower * 5f, ForceMode.Impulse);
+            #endregion
+
+            if (shootPower < 100f)
+            {
+                shootPower += Time.deltaTime * 50f;
+
+
+                // if (joint != null)
+                // {
+                //     Destroy(GetComponent<SpringJoint>());
+                // }
+            }
+        }
+
+        if (Input.GetButtonUp("Fire2") && isShooting == false)
+        {
+            Shoot(shootPower);
+        }
+
+        //rb.AddForce(1, gravityValue, 1);
+
+        float horizontaInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontaInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        //direction.position = transform.position;
+
+        transform.parent.Find("playerLookAt").position = new Vector3(transform.position.x, 2.4f, transform.position.z);
+
+        if (movementDirection != Vector3.zero)
+        {
+            // Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            //Transform direction = transform.Find("Direction");
+            direction.forward = movementDirection;
+
+            //     // direction.rotation = Quaternion.RotateTowards(direction.rotation, toRotation, 10f * Time.deltaTime);
+
+                if (!isMovingBall)
+                {
+                    Move(moveSpeed);
+                }
+            //     rb.AddForce(new Vector3(horizontaInput, 0f, verticalInput) * moveSpeed, forceModeMovement);
+        }
+
+        //moveSpeed = 12f;
+
+        //if (Input.GetButton("Fire3"))
+        // if (Input.GetKey(KeyCode.LeftShift))
+        // {
+        //     moveSpeed = moveSpeed + 10f;
+        // }
+        // else {
+        //     moveSpeed = 300f;
+        // }
+        // if (/*isMoving && */!isMovingBall)
+        // {
+        //     Move(5f);
+        // }
+
+        //transform.Find("Direction").Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        //print(rb.velocity.magnitude);
+
+        //rb.AddForce(new Vector3(horizontaInput * moveSpeed, gravityValue, verticalInput * moveSpeed), ForceMode.Acceleration);
+
+        //rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        //rb.velocity.y += gravityValue /** Time.deltaTime*/;
+
+        //print("Velocity (magnitude): " + rb.velocity.magnitude);
+
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            //rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+            //rb.AddForce(new Vector3(horizontaInput * moveSpeed, gravityValue, verticalInput * moveSpeed), /*ForceMode.Acceleration*/forceModeMovement);
+        }
+    }
+
+    void ResetIsShooting()
+    {
+        isShooting = false;
+        shootPower = 20f;
+    }
+
+    void Shoot(float force)
+    {
+        //TODO: Indentify why sometimes the SpringJoint won't destroyed when Shoot
+        Destroy(GetComponent<SpringJoint>());
+        Destroy(GetComponent<SpringJoint>());
+        Destroy(GetComponent<SpringJoint>());
+        isShooting = true;
+        rb.AddForce(direction.forward * Time.deltaTime * force * 50f, forceModeShoot /*ForceMode.Impulse*/);
+        rb.AddForce(direction.up * Time.deltaTime * force * upForce, forceModeShoot/*ForceMode.Impulse*/);
+
+        //Curve Test
+        //rb.AddForceAtPosition(direction.right * Time.deltaTime * 500f, transform.position, ForceMode.VelocityChange/*ForceMode.Impulse*/);
+
+        print("Shoot! Force: " + force);
+        Invoke("ResetIsShooting", 1f);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().EnablePlayerToRun();
+    }
+
+    public void Move(float force, float interval = 0.8f)
+    {
+        if (!isMovingBall && rb.velocity.magnitude < maxSpeed)
+        {
+            isMovingBall = true;
+            Vector3 velocity = direction.forward * Time.deltaTime * force;
+            //velocity.y += gravityValue * Time.deltaTime;
+            rb.AddForce(velocity, forceModeMovement/*ForceMode.Impulse*/);
+           // rb.AddForce(direction.up * Time.deltaTime * force * 20f, forceModeMovement/*ForceMode.Impulse*/);
+            print("Move! Force: " + force);
+            Invoke("ResetIsMovingBall", interval);
+        }
+    }
+
+    void ResetIsMovingBall()
+    {
+        isMovingBall = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "GoalCollider")
+        {
+            rb.drag = 1;
+            print("Goal!");
+        }
+
+        // if (other.name == "Foot")
+        // {
+        //     Move(moveSpeed);
+        // }
+    }
+}
